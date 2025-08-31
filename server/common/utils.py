787a11424655b0843caf5d2943d6f019b97a7114
claram97 <cruano@fi.uba.ar>
2025-08-31 17:46:01 -0300

    Restore number of winners

--- server/common/utils.py
+++ server/common/utils.py
@@ -6,9 +6,9 @@ import time
 """ Bets storage location. """
 STORAGE_FILEPATH = "./bets.csv"
 """ Simulated winner number in the lottery contest. """
-# LOTTERY_WINNER_NUMBER = 7574
+LOTTERY_WINNER_NUMBER = 7574
 
-LOTTERY_WINNER_NUMBER = 1342
+# LOTTERY_WINNER_NUMBER = 1342
 
 """ A lottery bet registry. """
 class Bet:

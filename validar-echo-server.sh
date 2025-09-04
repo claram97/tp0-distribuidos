7e8b5d8965c865a5ba340eafcade12adad7c2904
cruano <cruano@fi.uba.ar>
2025-09-04 14:50:31 -0300

    Remove sudo from validar echo server script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -11,7 +11,7 @@ MESSAGE="Hello, server!"
 # Ejecuto un contenedor que se elimina al finalizar la ejecución.
 # El contenedor usa una imágen de alpine y ejecuta netcat con espera de 1s.
 # El resultado se guarda en la variable RESPONSE.
-RESPONSE=$(sudo docker run --rm --network "$NETWORK_NAME" alpine sh -c "echo '$MESSAGE' | nc -w 1 '$SERVER_NAME' '$SERVER_PORT'")
+RESPONSE=$(docker run --rm --network "$NETWORK_NAME" alpine sh -c "echo '$MESSAGE' | nc -w 1 '$SERVER_NAME' '$SERVER_PORT'")
 
 if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
